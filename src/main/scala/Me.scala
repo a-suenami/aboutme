a3df45a0a28bdb5ceaca029448513897c0c7bf40
Akira SUENAMI <a.suenami@gmail.com>
2015-03-13 19:36:57 +0900

    Add "My name is Akira SUENAMI."

--- src/main/scala/Me.scala
+++ src/main/scala/Me.scala
@@ -1,10 +1,29 @@
 import java.util.Date
 
 class Me(val firstName: FirstName, val lastName: LastName) {
-  def favoriteThings(): Array[String] = {
+  def fullName: FullName = {
+    return FullName(firstName, lastName)
+  }
+
+  def favoriteThings: Array[String] = {
     return Array("Test-driven development", "Domain-driven design")
   }
 }
 
-case class FirstName(val value: String)
-case class LastName(val value: String)
+case class FirstName(val value: String) {
+  override def toString: String = {
+    return value
+  }
+}
+
+case class LastName(val value: String) {
+  override def toString: String = {
+    return value
+  }
+}
+
+case class FullName(val firstName: FirstName, val lastName: LastName) {
+  override def toString: String = {
+    return firstName.toString + " " + lastName.toString
+  }
+}
--- src/test/scala/MeSpec.scala
+++ src/test/scala/MeSpec.scala
@@ -1,6 +1,14 @@
 import org.scalatest._
 
 class MySpec extends FlatSpec with Matchers {
+  "My name" should "be Akira SUENAMI." in {
+    val me = new Me(
+      FirstName("Akira"),
+      LastName("SUENAMI")
+    )
+    me.fullName.toString should be ("Akira SUENAMI")
+  }
+
   "I" should "like Test-driven development." in {
     val me = new Me(
       FirstName("Akira"),
